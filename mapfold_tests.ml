75aca16a15cbb384dcedfa30671dd280cb285675
CS51 Heads <cs51heads@gmail.com>
2020-01-27 02:28:29 +0000

    Automated distribution code deploy

---
+++ mapfold_tests.ml
@@ -0,0 +1,18 @@
+(* 
+			 CS 51 Problem Set 2
+	    Higher Order Functional Programming -- Testing
+ *)
+
+open Mapfold ;;
+
+open Test_simple ;;      (* a really simple unit testing framework *)
+  
+let test () =
+  unit_test ((negate_all []) = []) "negate_all empty";
+  unit_test ((negate_all [1; -2; 0]) = [-1; 2; 0]) "negate_all mixed";
+
+  (*  Additional tests go here... *)
+
+  () ;;
+
+test ();;
